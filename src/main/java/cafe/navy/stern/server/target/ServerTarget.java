5857830a261d2437588fb24cfb817a64fa5d14b1
kadenscott <github.com@kaden.sh>
2022-09-11 18:10:14 -0400

    feat: client/target

--- src/main/java/cafe/navy/stern/server/target/ServerTarget.java
+++ src/main/java/cafe/navy/stern/server/target/ServerTarget.java
@@ -1,4 +1,4 @@
-package cafe.navy.stern.server.allocator;
+package cafe.navy.stern.server.target;
 
 import cafe.navy.stern.server.Server;
 import cafe.navy.stern.server.data.ServerData;
@@ -7,6 +7,20 @@ import cafe.navy.stern.server.settings.ServerSettings;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * {@code ServerTarget} represents an interface that can interact with a server target. A "server target" is essentially
+ * any system that can run a server -- Pterodactyl, k8s, docker, etc.
+ * <p>
+ * A {@code ServerTarget} is responsible for knowing how to:
+ * <ul>
+ *     <li>start and stop server instances</li>
+ *     <li>telling a server to load and unload player data</li>
+ *     <li>querying various metadata</li>
+ * </ul>
+ *
+ * @param <T> the server settings type
+ * @param <U> the server data type
+ */
 public interface ServerTarget<T extends ServerSettings, U extends ServerData> {
 
     CompletableFuture<Void> start(final Server<T, U> server);
